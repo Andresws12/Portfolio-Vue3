e017671e35eb9c1ef0d4ea05b581ed945af647e2
andresws12 <andreshernandez12.1995@gmail.com>
2023-08-21 20:03:22 +0200

    Remove unnecessary Icon

--- src/components/common/ButtonTranslation.vue
+++ src/components/common/ButtonTranslation.vue
@@ -1,12 +1,10 @@
 <template>
   <button @click="changeLanguage()">
-    <font-awesome-icon :icon="iconType.SOLID + ' ' + iconName.GLOBE" />
     <span>{{ $i18n.locale }}</span>
   </button>
 </template>
 <script lang="ts" setup>
 import { changeLanguage } from '@/utils/functionUtils/localizationFunctions';
-import { iconType, iconName } from '@/models/icons/fontawesome/iconsDictionary';
 </script>
 <style lang="scss" scoped>
 .button-translations {
