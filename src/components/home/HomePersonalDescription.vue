9aaf766be35f550b3cff484894ac9ee63601d393
andresws12 <andreshernandez12.1995@gmail.com>
2023-10-08 23:18:07 +0200

    Add personal description

---
+++ src/components/home/HomePersonalDescription.vue
@@ -0,0 +1,26 @@
+<template>
+  <section data-cy="home-section" class="home-personal-description">
+    <article>
+      <h2>{{ $t('views.home.homeDescriptionPersonal.descriptionWho') }}</h2>
+      <p>
+        {{ $t('views.home.homeDescriptionPersonal.descriptions.paragraph1') }}
+      </p>
+      <p>
+        {{ $t('views.home.homeDescriptionPersonal.descriptions.paragraph2') }}
+      </p>
+    </article>
+  </section>
+</template>
+
+<script lang="ts" setup>
+// Code
+</script>
+
+<style lang="scss" scoped>
+//Styles
+.home-personal-description {
+  background-color: $main-color;
+  color: $main-color-dark;
+  padding: 25px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <home-description />
+  <home-personal-description />
 </template>
 
 <script lang="ts" setup>
 import HomeDescription from '@/components/home/HomeDescription.vue';
+import HomePersonalDescription from '@/components/home/HomePersonalDescription.vue';
 </script>
